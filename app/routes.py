05a6ed31bf239c212133705d3693c0e4c2086b72
Haku <polina231003@mail.ru>
2023-05-20 00:49:55 +0300

    db queries and request form

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from flask import render_template
-from app import app
+from flask import render_template, request
+from app import app, db
+from app.models import Day, Event
 
 
 @app.route('/')
@@ -9,11 +10,33 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/index/<date>')
+@app.route('/index/<date>', methods=["POST", "GET"])
 def day(date):
     d, m, y = date.split('.')
+    params_post = {}
+    for p in request.form:
+        params_post[p] = request.form[p]
+
+    date_req = '/'.join((d, str(int(m) + 1), y))
+    day_req = Day.query.filter(Day.date == date_req).first()
+    if 'name' in params_post:
+        name = params_post['name']
+        if day_req is None:
+            new_day = Day(date=date_req)
+            db.session.add(new_day)
+            db.session.commit()
+            day_req = Day.query.filter(Day.date == date_req).first()
+        new_event = Event(name=name, day_id=day_req.id)
+        if 'time' in params_post:
+            time = params_post['time']
+            new_event = Event(time=time, name=name, day_id=day_req.id)
+        db.session.add(new_event)
+        db.session.commit()
+
     months = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня',
       'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря']
     m = months[int(m)]
     events = []
-    return render_template('day.html', day=d, month=m, year=y, events=events)
+    if day_req is not None:
+        events = day_req.events
+    return render_template('day.html', day=d, month=m, year=y, events=events, date=date)
